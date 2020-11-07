39aaf23ba39034e221f8dec89dc76b5759f787e0
DewanshJaiswal <72195673+DewanshJaiswal@users.noreply.github.com>
2020-11-07 22:36:50 +0530

    Add files via upload

---
+++ bob.js
@@ -0,0 +1,30 @@
+class Bob {
+    constructor(x, y, radius) {
+      var options = {
+          'restitution':0.3,
+          'friction':0.5,
+          'density':1.2,
+          'isStatic':true
+      }
+      this.body = Bodies.circle(x, y, radius, options);
+      this.radius = radius;
+      
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      ellipseMode(RADIUS);
+      strokeWeight(4);
+      stroke("pink");
+  
+      fill("purple");
+      ellipse(0, 0, this.radius,this.radius);
+      pop();
+    }
+  };
+  
